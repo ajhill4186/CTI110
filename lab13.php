5f5ea2b849c2a9cc02c86200ae547578152256d4
ajhill <hillalexander640@gmail.com>
2020-12-10 00:14:23 -0500

    Im a little boy who likes berrys and  cream.

--- lab13.php
+++ lab13.php
@@ -15,61 +15,54 @@
 <body>
     
     <?php
+// all this for a managers name ;)
+function getManagerName() {
+    $connect = mysqli_connect("localhost", "cti110", "wtcc", "mydatabase");
+    $userQuery = "SELECT firstName, lastName FROM personnel WHERE jobTitle = 'Manager'";
+    $result = mysqli_query($connect, $userQuery);
 
-$server = "localhost";
-$user = "cti110";
-$pw = "wtcc";
-$db = "mydatabase";
-$connect=mysqli_connect($server, $user, $pw, $db);
-    if( !$connect) {
-        die("ERROR: Cannot connect to database $db on server $server 
-            using user name $user (".mysqli_connect_errno().
-            ", ".mysqli_connect_error().")");
+    if(!$connect) {
+        die("ERROR: Cannot connect to database (".mysqli_connect_errno() . ", " . mysqli_connect_error() . ")");
     }
-$userQuery = "SELECT firstName, lastName FROM personnel WHERE jobTitle = 'Manager'";
-$result = mysqli_query($connect, $userQuery);
     if (!$result) {
-        die("Could not successfully run query ($userQuery) from $db: " .	
-            mysqli_error($connect) );
+        die("Could not successfully run query" . mysqli_error($connect) );
     }
-    if (mysqli_num_rows($result) == 0) {
-        print("No records found with query $userQuery");
-    } else { 
-        while ($row = mysqli_fetch_assoc($result))
-			{
-				$managerName = $row['firstName'] . " " . $row['lastName'];
-			}
-     }
-            mysqli_close($connect);   // close the connection
-        
+    $row = mysqli_fetch_assoc($result);
+    mysqli_close($connect);
+    return $row['firstName'] . " " . $row['lastName'];
+}
+
 $name = $_POST['name'];
 $phonenumber = $_POST['phonenumber'];
 $adultTicket = $_POST['adultticket'];
 $childTicket = $_POST['childticket'];
 $date = $_POST['date'];
 
-
-$adultCost = 35 * $adultTicket;
-$childCost = 30 * $childTicket;
-$subTotal = $adultCost + $childCost;
-$tax = $subTotal * 0.07;
-if ($adultTicket + $childTicket <= 5) {
-    $totalCost = $subTotal + $tax + 1.0 * ($adultTicket + $childTicket);
-} else {
-    $totalCost = $subTotal + $tax + 0.5 * ($adultTicket + $childTicket);
+// calculates the cost of the concert ;)
+function calcCost($adult, $child) {
+    global $subTotal, $tax, $totalCost;
+    $subTotal = (35 * $adult) + (30 * $child);
+    $tax = $subTotal * 0.07;
+    $totalCost = $subTotal + $tax + (($adult + $child <= 5) ? 1.0 : 0.5) * ($adult + $child);
 }
-print("<div class = \"mainBody\"><h1 class=\"mainheader\">Concert Ticket Order Summary</h1>");
-print("<p>Name: " . $name . "</p>");
-print("<p>Phone number: " . $phonenumber . "</p>");
-print("<p>Adult tickets: " . $adultTicket . "</p>");
-print("<p>Child tickets: " . $childTicket . "</p>");
-print("<p>Date: " . $date . "</p>");
-print("<p>Subtotal: $" . number_format($subTotal,2) . "</p>");
-print("<p>Tax: $" . number_format($tax,2) . "</p>");
-print("<p>Total cost: $" . number_format($totalCost,2) . "</p>");
-print("<p>Please contact the manager if you have question: " . "</p>");
-print("<p>" . $managerName . "</p>");
-print("<p>Thanks " . $name . " for using this program!" . "</p>");
+
+// prints the body of the reciept ;)
+function printReciept() {
+    return "<div class = \"mainBody\"><h1 class=\"mainheader\">Concert Ticket Order Summary</h1>" . 
+    "<p>Name: " . $GLOBALS['name'] . "</p>" . 
+    "<p>Phone number: " . $GLOBALS['phonenumber'] . "</p>" . 
+    "<p>Adult tickets: " . $GLOBALS['adultTicket'] . "</p>" . 
+    "<p>Child tickets: " . $GLOBALS['childTicket'] . "</p>" . 
+    "<p>Date: " . $GLOBALS['date'] . "</p>" . 
+    "<p>Subtotal: $" . number_format($GLOBALS['subTotal'],2) . "</p>" . 
+    "<p>Tax: $" . number_format($GLOBALS['tax'],2) . "</p>" . 
+    "<p>Total cost: $" . number_format($GLOBALS['totalCost'],2) . "</p>" . 
+    "<p>Please contact the manager if you have question:</p>" . 
+    "<p>" . getManagerName() . "</p>" . 
+    "<p>Thanks " . $GLOBALS['name'] . " for using this program!</p>";
+}
+calcCost($adultTicket, $childTicket);
+echo printReciept();
 ?>
 <footer class="returnbutton">
     <p><a href="lab13_main.html" class="buttonlink">Return to Main Page</a></p>
